64e2a107e1ca0ec770d5bb2f7e1cb22fd43d5161
x1210x <x1210x@gmail.com>
2022-09-29 10:34:50 +0900

    컬러

--- app/src/main/java/com/example/composetemplate/ui/common/ErrorScreen.kt
+++ app/src/main/java/com/example/composetemplate/ui/common/ErrorScreen.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.composetemplate.R
@@ -20,7 +21,7 @@ fun ErrorScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black.copy(0.3f))
+            .background(MaterialTheme.colorScheme.errorContainer)
     ) {
         Column(
             modifier = Modifier.align(Alignment.Center),
@@ -29,10 +30,12 @@ fun ErrorScreen() {
             Image(
                 modifier = Modifier.size(100.dp),
                 painter = painterResource(id = R.drawable.ic_error_outline),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onErrorContainer),
                 contentDescription = "error"
             )
             Text(
-                text = "에러가 발생했습니다."
+                text = "에러가 발생했습니다.",
+                color = MaterialTheme.colorScheme.onErrorContainer
             )
         }
     }
--- app/src/main/java/com/example/composetemplate/ui/common/LoadingScreen.kt
+++ app/src/main/java/com/example/composetemplate/ui/common/LoadingScreen.kt
@@ -4,18 +4,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LoadingScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black.copy(0.3f))
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center),
+            color = MaterialTheme.colorScheme.primary
+        )
     }
 }
