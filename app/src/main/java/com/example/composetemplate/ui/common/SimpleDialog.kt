805a990592df5dcabe15e13ca362e5465f363f18
x1210x <x1210x@gmail.com>
2022-09-30 14:59:50 +0900

    dialog

--- app/src/main/java/com/example/composetemplate/ui/common/SimpleDialog.kt
+++ app/src/main/java/com/example/composetemplate/ui/common/SimpleDialog.kt
@@ -1,15 +1,9 @@
 package com.example.composetemplate.ui.common
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SimpleDialog(
@@ -21,27 +15,22 @@ fun SimpleDialog(
     onOkClick: () -> Unit = { },
     onCancelClick: () -> Unit = { }
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0x33000000))
-    ) {
-        AlertDialog(modifier = Modifier.align(Alignment.Center),
-            onDismissRequest = onDismissRequest,
-            title = if (title != null) {
-                { Text(text = title) }
-            } else null,
-            text = { Text(text = message) },
-            confirmButton = {
-                TextButton(onClick = onOkClick) {
-                    Text(text = okText)
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = if (title != null) {
+            { Text(text = title) }
+        } else null,
+        text = { Text(text = message) },
+        confirmButton = {
+            TextButton(onClick = onOkClick) {
+                Text(text = okText)
+            }
+        }, dismissButton = if (cancelText != null) {
+            {
+                TextButton(onClick = onCancelClick) {
+                    Text(text = cancelText)
                 }
-            }, dismissButton = if (cancelText != null) {
-                {
-                    TextButton(onClick = onCancelClick) {
-                        Text(text = cancelText)
-                    }
-                }
-            } else null)
-    }
+            }
+        } else null
+    )
 }
