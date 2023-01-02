ef03b48cff9859be9f83bf35e8c1864ffa8b25ee
x1210x <x1210x@gmail.com>
2023-01-02 16:07:11 +0900

    version catalog 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,19 +89,46 @@ dependencies {
     implementation(libs.accompanist.webview)
 
     // Compose
-    implementation(libs.androidx.compose.bom)
-    //implementation(libs.androidx.compose.ui)
-    //implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.compose.material3)
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
 
-    // TODO : Android Studio Preview support (Build fix)
-//    implementation(libs.androidx.compose.ui.tooling.preview)
-//    debugImplementation(libs.androidx.compose.ui.tooling)
+    // Choose one of the following:
+    // Material Design 3
+    implementation(libs.androidx.compose.material3)
+    // or Material Design 2
+    //implementation(libs.androidx.compose.material)
+    // or skip Material Design and build directly on top of foundational components
+    // implementation("androidx.compose.foundation:foundation")
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    // implementation("androidx.compose.ui:ui")
+
+    // Android Studio Preview support
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // UI Tests
+    // androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    // debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Optional - Included automatically by material, only add when you need
+    // the icons but not the material library (e.g. when using Material3 or a
+    // custom design system based on Foundation)
+    // implementation("androidx.compose.material:material-icons-core")
+    // Optional - Add full set of material icons
+    // implementation("androidx.compose.material:material-icons-extended")
+    // Optional - Add window size utils
+    // implementation("androidx.compose.material3:material3-window-size-class")
 
     // Optional - Integration with activities
     implementation(libs.androidx.activity.compose)
     // Optional - Integration with ViewModels
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Optional - Integration with LiveData
+    // implementation("androidx.compose.runtime:runtime-livedata")
+    // Optional - Integration with RxJava
+    // implementation("androidx.compose.runtime:runtime-rxjava2")
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.constraint.layout.compose)
